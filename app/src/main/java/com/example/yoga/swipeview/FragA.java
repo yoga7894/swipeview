72ce6ca40b4da84e1720dff37077dea1ef2c1852
YOGASUBHAKARABABU <subhakarababu@gmail.com>
2017-12-20 14:04:18 +0530

    first commit

---
+++ app/src/main/java/com/example/yoga/swipeview/FragA.java
@@ -0,0 +1,21 @@
+package com.example.yoga.swipeview;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * Created by Yoga on 8/24/2017.
+ */
+
+public class FragA extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.frag_a,container,false);
+    }
+}
